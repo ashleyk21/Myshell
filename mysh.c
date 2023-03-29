2bc44e6f2c350f474f74c418412734364120f649
ashleyk21 <115744463+ashleyk21@users.noreply.github.com>
2023-03-29 10:22:36 -0400

    Update mysh.c

--- mysh.c
+++ mysh.c
@@ -8,7 +8,7 @@
 #include <dirent.h>
 #include <regex.h>
 
-#define MAX_LINE_LENGTH 1024
+#define MAX_COMMAND_LENGTH 1024
 #define MAX_ARGUMENTS 32
 #define MAX_TOKENS 64
 #define MAX_PATH_LENGTH 4096
@@ -22,9 +22,8 @@ typedef struct {
     // args[5] always NULL
     char *args[6];
 
-	// TRUE if the process is background, FALSE otherwise
     int isBackground;
-    int numOfArgs;
+    int amountofArgs;
 } Command;
 
 int execute_command(char *tokens[]);
@@ -58,24 +57,24 @@ void parse_command(char* command, char** arguments) {
 }
 
 // Checks if wildcards are present (0 wildcards, -1 no wildcards)
-int wildcardCheck(Command* myCommand, char currentDir[]) {
+int wildcardCheck(Command* ourCommand, char currentDir[]) {
 
 	// If the * symbol is present in the command
-    if (myCommand->args[0][0] == '*') {
+    if (ourCommand->args[0][0] == '*') {
         printf("Cannot use wildcard in the command.\n");
         return -1;
     }
 
 	// Loop that checks if each argument has the wildcard symbol and acts accordingly
     int i;
-    for (i = 1; i <= myCommand->numOfArgs; i++) {
+    for (i = 1; i <= ourCommand->amountofArgs; i++) {
 
 		// If the argument contains a wildcard, the argument is substituded with all the possible combinations
-        if (myCommand->args[i][0] == '*') {
-            char wildcard[strlen(myCommand->args[i])];
-            strcpy(wildcard, (myCommand->args[i]) + 1); // +1 excludes wildcard (*)
+        if (ourCommand->args[i][0] == '*') {
+            char wildcard[strlen(ourCommand->args[i])];
+            strcpy(wildcard, (ourCommand->args[i]) + 1); // +1 excludes wildcard (*)
             regex_t regex;
-            regcomp(&regex, strcat(wildcard, "$"), 0); // Regexp example: "g.txt$"
+            regcomp(&regex, strcat(wildcard, "$"), 0); 
 
 			// Open current directory and get all entities
             struct dirent *ent;
@@ -88,7 +87,7 @@ int wildcardCheck(Command* myCommand, char currentDir[]) {
                     arg = (char*) malloc(strlen(ent->d_name) + 1);
                     strcpy(arg, ent->d_name);
 					// Substitutes the argument containing the wildcard with the new filename
-                    myCommand->args[i] = arg;
+                    ourCommand->args[i] = arg;
                     // We only substitute one filename in this loop and add the rest later, hence break
 					break;
                 }
@@ -97,12 +96,12 @@ int wildcardCheck(Command* myCommand, char currentDir[]) {
 			// If no matching filename was found
             if (ent == NULL) {
                 int j = i;
-                while (j <= myCommand->numOfArgs) {
+                while (j <= ourCommand->amountofArgs) {
                     // Remove the wildcard argument and move the rest of the arguments one position backwards
-                    myCommand->args[j] = myCommand->args[j + 1];
+                    ourCommand->args[j] = ourCommand->args[j + 1];
                     j++;
                 }
-                (myCommand->numOfArgs)--;
+                (ourCommand->amountofArgs)--;
                 i--;
                 continue; // Next instruction
             }
@@ -110,12 +109,12 @@ int wildcardCheck(Command* myCommand, char currentDir[]) {
 			// Continue checking for matching filenames and store them in empty args[] slots (still max 4 args)
             while ((ent = readdir(dir)) != NULL) {
                 if (regexec(&regex, ent->d_name, 0, NULL, 0) == 0) {
-                    if (myCommand->numOfArgs < 4) {
-                        (myCommand->numOfArgs)++;
+                    if (ourCommand->amountofArgs < 4) {
+                        (ourCommand->amountofArgs)++;
                         arg = (char*) malloc(strlen(ent->d_name) + 1);
                         strcpy(arg, ent->d_name);
-                        myCommand->args[myCommand->numOfArgs] = arg;
-                        myCommand->args[myCommand->numOfArgs + 1] = NULL;
+                        ourCommand->args[ourCommand->amountofArgs] = arg;
+                        ourCommand->args[ourCommand->amountofArgs + 1] = NULL;
                     } else {
                         printf("Too many arguments given.(4 max)\n");
                         closedir(dir);
@@ -166,11 +165,10 @@ int execute_command(char** arguments) {
 }
 
 
-
 int main(int argc, char *argv[]) {
     FILE *input_file;
-    char line[MAX_LINE_LENGTH];
-    char command[MAX_LINE_LENGTH];
+    char line[MAX_COMMAND_LENGTH];
+    char command[MAX_COMMAND_LENGTH];
     char *tokens[MAX_TOKENS];
     int num_tokens;
     int status = 0;
@@ -186,7 +184,7 @@ int main(int argc, char *argv[]) {
             perror("fopen");
             exit(1);
         }
-        while (fgets(line, MAX_LINE_LENGTH, input_file) != NULL) {
+        while (fgets(line, MAX_COMMAND_LENGTH, input_file) != NULL) {
             num_tokens = 0;
             tokens[num_tokens] = strtok(line, " \t\n");
             while (tokens[num_tokens] != NULL && num_tokens < MAX_TOKENS-1) {
@@ -204,7 +202,7 @@ int main(int argc, char *argv[]) {
         printf("Welcome to my shell!\n");
         while (1) {
             print_prompt(status);
-            if (fgets(line, MAX_LINE_LENGTH, stdin) == NULL) {
+            if (fgets(line, MAX_COMMAND_LENGTH, stdin) == NULL) {
                 printf("\n");
                 break;
             }
